bdf0141e533734978dd75bbc8e74c50d86a08750
sk-whitedad <senkos@mail.ru>
2023-05-16 02:32:19 +0300

    gitignore

--- MyPhotoDreamApp.Domain/ViewModels/Order/UploadFilesViewModel.cs
+++ MyPhotoDreamApp.Domain/ViewModels/Order/UploadFilesViewModel.cs
@@ -1,10 +1,10 @@
 ﻿using MyPhotoDreamApp.Domain.Entity;
-using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace MyPhotoDreamApp.Domain.ViewModels.Order
 {
